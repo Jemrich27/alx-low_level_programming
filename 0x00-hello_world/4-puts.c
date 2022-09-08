c30ce8d53cab0551bc2e0449025f59d391228867
Jemrich27 <yvonnebestinice@gmail.com>
2022-09-08 14:50:09 -0700

    mill multilingual codes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 /**
- * Main- Serves as the entry point
- * for the program
+ * Main- Serves as the entry point for the program
  *
  * Return: By default returns zero if no errors otherwise returns nin-zero value.
  */
+
 int main(void)
 {
-	puts("\" Programming is like building a multilingual puzzle");
-	return (0);
+
+puts("\"Programming is like building a multilingual puzzle");
+	
+ return (0);
 }
 
