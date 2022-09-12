e2321f45b42c4197fee9f207177563f62e468b7d
Jemrich27 <yvonnebestinice@gmail.com>
2022-09-12 14:47:12 -0700

    momo nyabo

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - Prints the alphabet in lower and then upper case.
+ * Rwturn: Always 0.
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a' ; letter <= 'z' ; letter++)
+
+		putchar(letter);
+	for (letter = 'A' ; letter <= 'Z' ; letter++)
+		putchar(letter);
+	putchar ('\n');
+
+	return (0);
+}
