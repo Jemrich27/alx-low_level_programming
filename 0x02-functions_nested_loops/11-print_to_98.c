241fe99dc4af059f10b8e20f155dbdc958f8232f
Jemrich27 <yvonnebestinice@gmail.com>
2022-09-19 02:35:33 -0700

    task11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - prints numbers
+ * @n: the first number to be counted
+ *
+ * Description: print all natural intergers to 98
+ * Return: returns the result of addition
+ */
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		for (n = n; n < 98; n++)
+			printf("%d, ", n);
+			printf("%d\n", 98);
+	}
+	else
+	{
+		for (n = n; n > 98; n--)
+			printf("%d, ", n);
+			printf("%d\n", 98);
+	}
+}
+
