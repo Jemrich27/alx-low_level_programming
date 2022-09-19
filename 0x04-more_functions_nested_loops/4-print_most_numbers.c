73f587b1f908e7873e6b0657c216b7fd1c119736
Jemrich27 <yvonnebestinice@gmail.com>
2022-09-19 10:24:32 -0700

    task4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_most_numbers - function to print numbers in string except 2 and 4
+ *
+ * Return: Function returns 0 on success
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i == 2 || i == 4)
+			continue;
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+
+}
